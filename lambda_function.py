cff709f368ca2d105ec52a6dff24b08970f419f5
Aleksandar Velkoski <hello@stks.io>
2016-03-17 19:49:57 -0500

    Add built-in intents

--- lambda_function.py
+++ lambda_function.py
@@ -1,16 +1,8 @@
-from __future__ import print_function
-from fred import Fred
-import inflect
+from skills import *
+from helpers import *
+import config
 
-inf = inflect.engine()
-fr = Fred(api_key='f2baf6f259b91363fc605dcae58bdcaf',response_type='dict')
-params = {
-         'limit':1,
-         'output_type':1,
-         'sort_order':'desc'
-         }
-
-
+# --------------------------- Lambda Function ----------------------------------
 def lambda_handler(event, context):
     """ Route the incoming request based on type (LaunchRequest, IntentRequest,
     etc.) The JSON body of the request is provided in the event parameter.
@@ -23,8 +15,7 @@ def lambda_handler(event, context):
     prevent someone else from configuring a skill that sends requests to this
     function.
     """
-    if (event['session']['application']['applicationId'] !=
-             "amzn1.echo-sdk-ams.app.8a876b3a-ca9b-4dca-b985-2dac4d47ae8c"):
+    if (event['session']['application']['applicationId'] != config.APP_ID):
          raise ValueError("Invalid Application ID")
 
     if event['session']['new']:
@@ -70,7 +61,9 @@ def on_intent(intent_request, session):
     if intent_name == "GetEconomicData":
         return get_economic_series(intent,session)
     elif intent_name == "AMAZON.HelpIntent":
-        return get_welcome_response()
+        return get_help_response()
+    elif intent_name =='AMAZON.StopIntent':
+        return get_stop_response()
     else:
         raise ValueError("Invalid intent")
 
@@ -82,82 +75,3 @@ def on_session_ended(session_ended_request, session):
     """
     print("on_session_ended requestId=" + session_ended_request['requestId'] +
           ", sessionId=" + session['sessionId'])
-    # add cleanup logic here
-
-# --------------- Functions that control the skill's behavior ------------------
-
-
-def get_welcome_response():
-    """ If we wanted to initialize the session to have some attributes we could
-    add those here
-    """
-
-    session_attributes = {}
-    card_title = "Welcome"
-    speech_output = "Welcome to FRED. " \
-                    "FRED provides information about economic data series. " \
-                    "For example, you can ask what is the most recent \
-                     value of GDP. "
-    # If the user either does not reply to the welcome message or says something
-    # that is not understood, they will be prompted again with this text.
-    reprompt_text = "Please tell me what economic data series you are interested in. " \
-                    "You can say GDP or Gross Domestic Product. "
-    should_end_session = False
-    return build_response(session_attributes, build_speechlet_response(
-        card_title, speech_output, reprompt_text, should_end_session))
-
-def get_economic_series(intent,session):
-    reprompt_text = None
-    session_attributes = {}
-    if 'Series' in intent['slots']:
-        series = intent['slots']['Series']['value']
-        if series.lower() in ['gdp','gross domestic product']:
-            res = fr.series.observations('GDP',params=params)
-            res = res[0]['value']
-
-            val = inf.number_to_words(int(res*1000000000))
-            #val = "12000"
-        speech_output = "GDP is " + val + \
-                        "."
-        should_end_session = True
-    else:
-        speech_output = "I could not find the economic data series. " \
-                        "Try again by asking for the value of GDP."
-        should_end_session = False
-
-    # Setting reprompt_text to None signifies that we do not want to reprompt
-    # the user. If the user does not respond or says something that is not
-    # understood, the session will end.
-    return build_response(session_attributes, build_speechlet_response(
-        intent['name'], speech_output, reprompt_text, should_end_session))
-
-# --------------- Helpers that build all of the responses ----------------------
-
-
-def build_speechlet_response(title, output, reprompt_text, should_end_session):
-    return {
-        'outputSpeech': {
-            'type': 'PlainText',
-            'text': output
-        },
-        'card': {
-            'type': 'Simple',
-            'title': 'SessionSpeechlet - ' + title,
-            'content': 'SessionSpeechlet - ' + output
-        },
-        'reprompt': {
-            'outputSpeech': {
-                'type': 'PlainText',
-                'text': reprompt_text
-            }
-        },
-        'shouldEndSession': should_end_session
-    }
-
-
-def build_response(session_attributes, speechlet_response):
-    return {
-        'version': '1.0',
-        'sessionAttributes': session_attributes,
-        'response': speechlet_response
-    }
