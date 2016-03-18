6ec94260f5ba3dc2305c30766ffd1bb77bdb9d2e
Aleksandar Velkoski <hello@stks.io>
2016-03-17 19:58:59 -0500

    Add config

---
+++ config.py
@@ -0,0 +1,21 @@
+APP_ID = ''
+
+API_KEY = ''
+RESPONSE_TYPE = 'dict'
+
+SEARCH_PARAMS = {'limit':5,'order_by':'search_rank','sort_order':'desc'}
+SERIES_PARAMS = {'limit':1,'output_type':1,'sort_order':'desc'}
+
+NO_SERIES_FOUND = "I could not find the economic data series.  \
+                   Try again."
+
+NO_SERIES_FOUND_LONG = "I could not find the economic data series.  \
+                   You can ask, what is the unemployment rate in Chicago, \
+                   or what is the Labor Force Participation Rate for Men."
+
+HELP =  "Ask FRED to tell you about an economic data series. \
+        You can ask about GDP, Unemployment Rate, Federal Funds Rate, \
+        Housing Starts, Money Stock, the Consumer Price Index, \
+        Nonfarm Payroll, Consumer Sentiment, and much more. \
+        For more information about available series, visit \
+        St. Louis Fed dot org"
