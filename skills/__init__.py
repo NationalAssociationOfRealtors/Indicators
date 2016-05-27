15b2c663b024e3885be62f92894dac241070bade
Aleksandar Velkoski <avelkoski@realtors.org>
2016-05-27 07:09:31 -0500

    Speech output to ssml

--- skills/__init__.py
+++ skills/__init__.py
@@ -13,24 +13,25 @@ def get_welcome_response():
     """ If we wanted to initialize the session to have some attributes we could
     add those here
     """
-
     session_attributes = {}
     card_title = "Welcome"
-    speech_output = "Welcome to Indicators, data about global economic activity. " \
-                    "What would you like to know?"
+    speech_output = config.WELCOME
     # If the user either does not reply to the welcome message or says something
     # that is not understood, they will be prompted again with this text.
-    reprompt_text = "Please tell me what data series you are interested in. " \
-                    "For example, you can say tell me about existing home sales, \
-                     or what is the value of Federal Debt as a Percent of GDP. "
+    reprompt_text = config.WELCOME_REPROMPT
     should_end_session = False
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
 
 def get_economic_series(intent,session):
     session_attributes = {}
+    card_title = "Your Question"
     try:
-        if 'Series' in intent['slots']:
+        if not 'value' in intent['slots']['Series']:
+            speech_output = config.PROMPT_AFTER_EMPTY
+            reprompt_text = config.REPROMPT_LONG
+            should_end_session = False
+        else:
             series = intent['slots']['Series']['value']
             series = series if not 's and p five hundred' in series else 's and p 500'
             searched = fr.series.search(series,params=config.SEARCH_PARAMS)
@@ -38,35 +39,33 @@ def get_economic_series(intent,session):
                 title, units, _id = searched[0]['title'], searched[0]['units'], searched[0]['id']
                 if u'\xa9' in title and _id not in APPROVED:
                     speech_output = config.COPYRIGHT
+                    reprompt_text = config.REPROMPT_SHORT
                     should_end_session = False
                 else:
                     units = dispatch_units(units)
                     res = fr.series.observations(_id,params=config.SERIES_PARAMS)[0]['value']
                     val = inf.number_to_words(float("%.2f" % float(res)))
-                    speech_output = title + " is " + val + " " + units + "."
+                    speech_output = '<speak>' + title + ' is ' + val + ' ' + units + r"<break time='0.45s'/>. " + config.PROMPT_AFTER_SUCCESS + '</speak>'
+                    reprompt_text = config.REPROMPT_SHORT
                     should_end_session = False
             else:
                 speech_output = config.NO_SERIES_FOUND
+                reprompt_text = config.REPROMPT_SHORT
                 should_end_session = False
-        else:
-            speech_output = config.NO_SERIES_FOUND_LONG
-            should_end_session = False
     except Exception:
         speech_output = config.ERROR
+        reprompt_text = config.REPROMPT_LONG
         should_end_session = True
-    reprompt_text = config.REPROMPT_SHORT
     # Setting reprompt_text to None signifies that we do not want to reprompt
     # the user. If the user does not respond or says something that is not
     # understood, the session will end.
     return build_response(session_attributes, build_speechlet_response(
-        intent['name'], speech_output, reprompt_text, should_end_session))
+        card_title, speech_output, reprompt_text, should_end_session))
 
 def get_help_response():
     session_attributes = {}
     card_title = "Help"
     speech_output = config.HELP
-    # If the user either does not reply to the welcome message or says something
-    # that is not understood, they will be prompted again with this text.
     reprompt_text = config.REPROMPT_SHORT
     should_end_session = False
     return build_response(session_attributes, build_speechlet_response(
@@ -76,7 +75,7 @@ def get_stop_response():
     session_attributes = {}
     reprompt_text = None
     card_title = "End"
-    speech_output = "Goodbye."
+    speech_output = config.GOODBYE
     should_end_session = True
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
